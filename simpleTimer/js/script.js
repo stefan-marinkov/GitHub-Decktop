1d7cea8c801b235d4e20d31d447f7f91f8c681b1
stefan-marinkov <72408345+stefan-marinkov@users.noreply.github.com>
2021-02-11 08:26:30 +0100

    simple Timer
    next simple project

---
+++ simpleTimer/js/script.js
@@ -0,0 +1,63 @@
+import {createEl, selectEl} from './create.js'
+
+const wrap = selectEl('.wrap')
+const burger = selectEl('.burger')
+
+// !toggle with burger
+
+burger.addEventListener('click', () => { 
+    wrap.classList.toggle('active')
+    burger.classList.toggle('toggle')
+})
+
+// CLOCK PARTS
+
+const start = createEl('button', 'btn')
+const input = createEl('input')
+input.setAttribute('value', '')
+input.setAttribute('placeholder', 'Insert Number')
+const clock = createEl('div', 'clock')
+
+
+wrap.appendChild(start)
+wrap.appendChild(input)
+wrap.appendChild(clock)
+start.textContent = 'START'
+
+
+start.addEventListener('click', () => {
+    
+    start.style.opacity = '0'
+    const rest = createEl('button', 'rest')
+    wrap.appendChild(rest)
+    rest.textContent = 'RESET'
+    let time = input.value * 60
+    setInterval(startTimer, 1000)
+
+function startTimer() {
+
+    
+    
+    let minute = Math.floor( time / 60)
+    let secund = time % 60
+
+    minute = minute<10 ? `0${minute}` : minute
+    secund = secund<10 ? `0${secund}` : secund
+
+    
+
+    if( time > 0) {
+        time --
+        clock.innerHTML = `<p>${minute}</p><p>:</p><p>${secund}</p>`
+        input.style.display = 'none'
+    } 
+        
+        rest.addEventListener('click', () => {
+            
+            location.reload()
+        })
+
+    }
+    
+startTimer()
+})
